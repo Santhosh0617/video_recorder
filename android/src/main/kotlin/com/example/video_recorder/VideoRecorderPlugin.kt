106e984fcca5988f2a8b106f17bdee576e94d9e4
Santhosh0617 <114073187+Santhosh0617@users.noreply.github.com>
2023-09-05 12:57:43 +0530

    Added Recording Option

--- android/src/main/kotlin/com/example/video_recorder/VideoRecorderPlugin.kt
+++ android/src/main/kotlin/com/example/video_recorder/VideoRecorderPlugin.kt
@@ -6,12 +6,14 @@ import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
+import java.io.IOException
 
 /** VideoRecorderPlugin */
 class VideoRecorderPlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
     private lateinit var activity: Activity
     private lateinit var mediaRecorder: MediaRecorder
     private var isRecording = false
+    private var outputFilePath: String? = null
 
     override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
         val channel = MethodChannel(binding.binaryMessenger, "video_recorder")
@@ -27,7 +29,7 @@ class VideoRecorderPlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
             }
             "stopRecording" -> {
                 stopRecording()
-                result.success(null)
+                result.success(outputFilePath)
             }
             else -> result.notImplemented()
         }
@@ -35,17 +37,50 @@ class VideoRecorderPlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
 
     private fun startRecording() {
         if (!isRecording) {
-            // Implement Android screen recording logic here
-            // You will need to use MediaRecorder or another suitable library
-            isRecording = true
+            mediaRecorder = MediaRecorder()
+            
+            // Set the source and output format
+            mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE)
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            
+            // Set the video and audio encoding parameters
+            mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264)
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+            
+            // Set the output file path
+            outputFilePath = getOutputFilePath() // Define this method to get a unique file path
+            mediaRecorder.setOutputFile(outputFilePath)
+            
+            try {
+                mediaRecorder.prepare()
+                mediaRecorder.start()
+                isRecording = true
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
         }
     }
 
     private fun stopRecording() {
         if (isRecording) {
-            // Stop the screen recording
-            isRecording = false
+            try {
+                mediaRecorder.stop()
+                mediaRecorder.release()
+                isRecording = false
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
+    }
+    
+    private fun getOutputFilePath(): String {
+        // Define a method to generate a unique output file path
+        // You can use Environment.getExternalStorageDirectory() to get the external storage directory
+        // and append a unique filename to it.
+        // For example:
+        // val dir = Environment.getExternalStorageDirectory()
+        // val fileName = "video_${System.currentTimeMillis()}.mp4"
+        // return File(dir, fileName).absolutePath
     }
 
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
