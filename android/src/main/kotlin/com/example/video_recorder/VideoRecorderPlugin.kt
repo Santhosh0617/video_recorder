0256c6cda36ad59b789f8df768628e2bd8b9482f
Santhosh0617 <114073187+Santhosh0617@users.noreply.github.com>
2023-09-05 13:19:12 +0530

    Corrected Recording

--- android/src/main/kotlin/com/example/video_recorder/VideoRecorderPlugin.kt
+++ android/src/main/kotlin/com/example/video_recorder/VideoRecorderPlugin.kt
@@ -1,12 +1,18 @@
 package com.example.video_recorder
 
 import android.app.Activity
+import android.content.pm.PackageManager
 import android.media.MediaRecorder
+import android.os.Environment
 import androidx.annotation.NonNull
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
+import java.io.File
 import java.io.IOException
+import java.util.UUID
 
 /** VideoRecorderPlugin */
 class VideoRecorderPlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
@@ -24,62 +30,88 @@ class VideoRecorderPlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: MethodChannel.Result) {
         when (call.method) {
             "startRecording" -> {
-                startRecording()
-                result.success(null)
+                if (startRecording()) {
+                    result.success(null)
+                } else {
+                    result.error("RECORDING_ERROR", "Failed to start recording", null)
+                }
             }
             "stopRecording" -> {
-                stopRecording()
-                result.success(outputFilePath)
+                val path = stopRecording()
+                if (path != null) {
+                    result.success(path)
+                } else {
+                    result.error("RECORDING_ERROR", "Failed to stop recording", null)
+                }
             }
             else -> result.notImplemented()
         }
     }
 
-    private fun startRecording() {
+    private fun startRecording(): Boolean {
         if (!isRecording) {
-            mediaRecorder = MediaRecorder()
-            
-            // Set the source and output format
-            mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE)
-            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
-            
-            // Set the video and audio encoding parameters
-            mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264)
-            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
-            
-            // Set the output file path
-            outputFilePath = getOutputFilePath() // Define this method to get a unique file path
-            mediaRecorder.setOutputFile(outputFilePath)
-            
-            try {
-                mediaRecorder.prepare()
-                mediaRecorder.start()
-                isRecording = true
-            } catch (e: IOException) {
-                e.printStackTrace()
+            if (checkPermissions()) {
+                mediaRecorder = MediaRecorder()
+                mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
+                mediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE)
+                mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+                mediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264)
+                mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+
+                outputFilePath = getOutputFilePath()
+                mediaRecorder.setOutputFile(outputFilePath)
+
+                try {
+                    mediaRecorder.prepare()
+                    mediaRecorder.start()
+                    isRecording = true
+                    return true
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
             }
         }
+        return false
     }
 
-    private fun stopRecording() {
+    private fun stopRecording(): String? {
         if (isRecording) {
             try {
                 mediaRecorder.stop()
+                mediaRecorder.reset()
                 mediaRecorder.release()
                 isRecording = false
+                return outputFilePath
             } catch (e: Exception) {
                 e.printStackTrace()
             }
         }
+        return null
     }
-    
+
+    private fun checkPermissions(): Boolean {
+        val permissions = arrayOf(
+            android.Manifest.permission.RECORD_AUDIO,
+            android.Manifest.permission.CAMERA,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+
+        val grantedPermissions = ArrayList<String>()
+        for (permission in permissions) {
+            if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(activity, permissions, 0)
+            } else {
+                grantedPermissions.add(permission)
+            }
+        }
+
+        return grantedPermissions.size == permissions.size
+    }
+
     private fun getOutputFilePath(): String {
-        // Define a method to generate a unique output file path
-        // You can use Environment.getExternalStorageDirectory() to get the external storage directory
-        // and append a unique filename to it.
-        // For example:
-        val dir = Environment.getExternalStorageDirectory()
-        val fileName = "video_${System.currentTimeMillis()}.mp4"
+        val dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES)
+        dir.mkdirs()
+        val fileName = "video_${UUID.randomUUID()}.mp4"
         return File(dir, fileName).absolutePath
     }
 
